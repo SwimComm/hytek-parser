d7b8f6c7dd5e1960cab7052063d172d1aa919588
Nino Maruszewski <nino.maruszewski@gmail.com>
2022-02-09 13:02:41 -0600

    fix: add `__all__` and other dunders

--- hytek_parser/__init__.py
+++ hytek_parser/__init__.py
@@ -1,4 +1,7 @@
 """A Hytek hy3 file parser."""
-from .parser import parse_hy3  # noqa: F401
+from .parser import parse_hy3
 
+__all__ = ["parse_hy3"]
+__author__ = "Nino Maruszewski"
+__license__ = "MIT"
 __version__ = "0.1.0"
