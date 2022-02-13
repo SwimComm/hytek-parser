116a30e76a9af5bf02c915c15d1c037e3a7d79c6
Nino Maruszewski <nino.maruszewski@gmail.com>
2022-02-12 23:30:53 -0600

    refactor: rename file

--- hytek_parser/__init__.py
+++ hytek_parser/__init__.py
@@ -1,6 +1,6 @@
 """A Hytek hy3 file parser."""
 from .export_csv import EVENT_RESULT_CSV_HEADER
-from .parser import parse_hy3
+from .hy3_parser import parse_hy3
 
 __all__ = ["parse_hy3", "EVENT_RESULT_CSV_HEADER"]
 __author__ = "Nino Maruszewski"
