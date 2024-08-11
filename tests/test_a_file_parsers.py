ae7e5c425485f353a407482603e03743a8d26d35
Mads Hansen <mads.hansen@marklogic.com>
2024-08-10 23:27:35 -0400

    Changing date format from dmy to mdy

---
+++ tests/test_a_file_parsers.py
@@ -0,0 +1,16 @@
+import unittest
+from datetime import datetime
+from hytek_parser.hy3.schemas import ParsedHytekFile
+from hytek_parser.hy3.line_parsers.a_file_parsers import a1_parser
+
+class TestA1Parser(unittest.TestCase):
+    
+    def test_date_created(self):
+        line = "A102Meet Entries             Hy-Tek, Ltd    Win-TM 8.0Ga  06272024 12:00 PMOakton Swim Team"
+        file = ParsedHytekFile()
+        opts = {}
+        result = a1_parser(line, file, opts)
+        return self.assertEqual(datetime.fromisoformat('2024-06-27T12:00:00'), result.date_created)
+    
+if __name__=='__main__':
+	unittest.main()
